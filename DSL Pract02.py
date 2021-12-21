a45eeffaf3980a73236fd53461d7664b27e2f604
Ciphe_R <89970617+Azadg0105@users.noreply.github.com>
2021-12-21 18:36:05 +0530

    Update DSL Pract02.py

--- DSL Pract02.py
+++ DSL Pract02.py
@@ -1,8 +1,15 @@
+# Write a Python program that computes the net amount of a bank account based a transaction
+# log from console input. The transaction log format is shown as following: D 100 W 200
+# (Withdrawal is not allowed if balance is going negative. Write functions for withdraw and deposit)
+# D means deposit while W means withdrawal.
+# Suppose the following input is supplied to the program:
+# D 300, D 300 , W 200, D 100 Then, the output should be: 500
+
 # To compute net amount of your Bank Account
 
 Amount = 0
 while True:
-  str = input("Enter transaction: ")
+  str = input("\nEnter Transaction (d/w for deposit/withdrawal followed by Amount): ")
   trans = str.split(" ")
 
   type = trans[0]
@@ -11,12 +18,15 @@ while True:
   if type == "D" or type == "d":
     Amount += amt
   elif type == "W" or type == "w":
-    Amount -= amt
+      if amt > Amount:
+          print("\nYour Account Balance is Low, Rs.",Amount," only.")
+      else:
+          Amount -= amt
   else:
     pass
 
-  str = input("Press Y to continue else press any key to End Transaction: ")
+  str = input("\nPress Y to continue else press any key to End Transaction: ")
   if not (str[0] =="Y" or str[0] =="y"):
     break
 
-print("Net amount: ", Amount)
+print("\nYour available Balance is Rs.",Amount," only.")
