cd573e676cc5eeffb9cf042f8d511de022ed5032
Ciphe_R <89970617+Azadg0105@users.noreply.github.com>
2021-12-21 18:11:20 +0530

    Update DSL Matrix Operation.py

--- DSL Matrix Operation.py
+++ DSL Matrix Operation.py
@@ -1,46 +1,103 @@
-row=int(input("Enter no. of Rows:"))
-col=int(input("Enter no. of Columns:"))
-matrix=[]
+# A-9 Write a Python program to compute following computation on matrix:
+# a)  Addition of two matrices
+# b)	Subtraction of two matrices
+# c)	Multiplication of two matrices
+# d)	Transpose of a matrix
 
-#matrix function
-def cmatrix():
-  for i in range(row):
-    a=[]
+row = int(input("Enter no. of Rows: "))
+col = int(input("Enter no. of Columns: "))
+
+matrix1 = []
+
+for i in range(row):
+    a  = []
     for j in range(col):
-      b=int(input())
-      a.append(b)
-    matrix.append(a)
-
-  for i in range(row):
+        a.append(int(input("Enter Elements of First Matrix: ")))
+    matrix1.append(a)
+for i in range(row):
     for j in range(col):
-      print(matrix[i][j],end=' ')
-    print()
-  return matrix
+        print(matrix1[i][j], end='  ')
+    print(end='\n')
 
-m=cmatrix()
-print(m)
-
+matrix2 = []
 
 for i in range(row):
-  for j in range(col):
-    if m[i][j]==0:
-      continue
-    else :
-      print(i,'\t',j,'\t', m[i][j])
-
-matrix=[]
-x=cmatrix()
-print(x)
-
-
+    a = []
+    for j in range(col):
+        a.append(int(input("Enter Elements of Second Matrix: ")))
+    matrix2.append(a)
 for i in range(row):
-  for j in range(col):
-    if x[i][j]==0:
-      continue
-    else :
-      print(i,'\t',j,'\t', x[i][j])
+    for j in range(col):
+        print(matrix2[i][j], end='  ')
+    print(end='\n')
 
+# addition
+res = []
 for i in range(row):
-  for j in range(col):
-    if m[i][j]!=0 and x[i][j]!=0:
-      print(i,'\t',j,'\t',m[i][j]+x[i][j])
+    s = []
+    for j in range(col):
+        b = int(matrix1[i][j] + matrix2[i][j])
+        s.append(b)
+    res.append(s)
+
+print("\nAddition of Two Matrices: ")
+for i in range(row):
+    for j in range(col):
+        print(res[i][j], end='  ')
+    print(end='\n')
+
+# subtraction
+res = []
+for i in range(row):
+    r = []
+    for j in range(col):
+        c = int(matrix1[i][j] - matrix2[i][j])
+        r.append(c)
+    res.append(r)
+
+print("\nSubtraction of Two Matrices: ")
+for i in range(row):
+    for j in range(col):
+        print(res[i][j], end='  ')
+    print(end='\n')
+
+# multiplication
+result = [[0 for i in range(row)] for j in range(col)]
+for i in range(row):
+    for j in range(col):
+        for k in range(row):
+            result[i][j] = result[i][j] + matrix1[i][k] * matrix2[k][j]
+
+print("\nMultiplication of Two Matrices: ")
+for i in range(row):
+    for j in range(col):
+        print(result[i][j], end='  ')
+    print(end='\n')
+
+# transpose of two matrices
+result = []
+for i in range(row):
+    t = []
+    for j in range(col):
+        trans = matrix1[j][i]
+        t.append(trans)
+    result.append(t)
+
+print("\nTranspose of Matrix1: ")
+for i in range(row):
+    for j in range(col):
+        print(result[i][j], end='  ')
+    print(end='\n')
+
+result = []
+for i in range(row):
+    t = []
+    for j in range(col):
+        trans = matrix2[j][i]
+        t.append(trans)
+    result.append(t)
+print("Transpose of Matrix2: ")
+for i in range(row):
+    for j in range(col):
+        print(result[i][j], end='  ')
+    print(end='\n')
